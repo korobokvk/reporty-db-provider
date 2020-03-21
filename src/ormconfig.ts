2d5ced47ec0bff9197d2b949e76326692852a64b
korobokvk <korobokvk@gmail.com>
2020-03-22 01:59:20 +0200

    upd db names

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,15 +1,15 @@
 import { ConnectionOptions } from 'typeorm'
 const dbUrl = process.env.DB_URL
 const isDev = process.env.DEV_ENV
-const passwordAndDb = isDev ? 'root' : 'root1234'
+const password = isDev ? 'root' : 'root1234'
 
 const config: ConnectionOptions = {
   type: 'postgres',
   host: dbUrl,
   port: 5432,
   username: 'root',
-  password: passwordAndDb,
-  database: passwordAndDb,
+  password: password,
+  database: 'database-1',
   entities: ['src/entity/**/*.ts'],
   migrations: ['src/migration/**/*.ts'],
   subscribers: ['src/subscriber/**/*.ts'],

c81c0e2d7ed81c6c0416737624c27ce58c778ee1
korobokvk <korobokvk@gmail.com>
2020-03-22 01:45:55 +0200

    upd

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM node:latest
 WORKDIR /usr/src/app
 COPY package.json package.json 
 RUN npm install && npm install typescript -g && npm install grpc
-ENV DB_URL=172.31.16.105
+ENV DB_URL=18.189.167.126
 COPY . /usr/src/app
 RUN tsc
 
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,13 +1,15 @@
 import { ConnectionOptions } from 'typeorm'
 const dbUrl = process.env.DB_URL
+const isDev = process.env.DEV_ENV
+const passwordAndDb = isDev ? 'root' : 'root1234'
 
 const config: ConnectionOptions = {
   type: 'postgres',
   host: dbUrl,
   port: 5432,
   username: 'root',
-  password: 'root',
-  database: 'root',
+  password: passwordAndDb,
+  database: passwordAndDb,
   entities: ['src/entity/**/*.ts'],
   migrations: ['src/migration/**/*.ts'],
   subscribers: ['src/subscriber/**/*.ts'],
